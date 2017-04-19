c72f770a1db3c0f62e1443bca1cf283bba976de8
julien da silva <julien.da-silva@hetic.netgit>
2017-04-19 19:53:54 +0200

    add button responsive

--- wp-content/themes/UD_theme/templates/template_accueil.php
+++ wp-content/themes/UD_theme/templates/template_accueil.php
@@ -5,7 +5,18 @@
 get_header();
 ?>
 
-<p>Page Accueil</p>
+<div class="accueil-background flex-container-accueil">
+    <div class="accueil-title-left flex-container-accueil-left">
+        <div class="left-line2-container">
+            <div class="line2"></div>
+        </div>
+        <div class="flex-container-left-column">
+            <h2 class="title text-white accueil-title">U&D est là pour tout vos travaux de gros oeuvres et d’isolation en Ile-de-France. Au service des entreprises et
+                des particuliers.</h2>
+            <button class="accueil-button">Nous contacter</button>
+        </div>
+    </div>
+</div>
 
 <?php
 get_footer();
