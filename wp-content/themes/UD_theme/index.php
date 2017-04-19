9990e418dcf054dd5cbe41654c7e2a6bf6634861
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-04-19 15:09:56 +0200

    index --> accueil

--- wp-content/themes/UD_theme/index.php
+++ wp-content/themes/UD_theme/index.php
@@ -1,11 +1,7 @@
 <?php
 get_header();
 ?>
-<p class="title">L’entreprise U&G est une équipe d’experts à votre écoute, <span class="text-important">qui vous conseille et vous oriente dans vos choix techniques</span> et répond au mieux à vos besoins</p>
-<p class="paragraphe">Spécialisée dans la rénovation complète ou partielle d’appartement et de maison, notre équipe d’ouvriers professionnels du bâtiment intervient pour tous les travaux nécessaires à l’intérieur de votre habitât.
 
-    <span class="text-important">Notre entreprise est à votre écoute</span> afin de discuter de votre projet de travaux. Nos devis sont en fourniture et pose (sauf demande contraire de votre part). Ainsi U&D gére l’intégralité de vos travaux, approvisionne et livre tous les matériels sur le chantier.</p>
-<p class="paragraphe">Here goes the content Here goes the content Here goes the content Here goes the content Here goes the content</p>
 <?php
 get_footer();
 ?>
