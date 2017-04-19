e8b345645a3d75f03b9176035b66dceefa0b73e8
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-04-19 15:07:27 +0200

    Adding templates structures

---
+++ wp-content/themes/UD_theme/templates/template_a-propos.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Template Name: A propos
+*/
+get_header();
+?>
+
+<div>Page A propos</div>
+
+<?php
+get_footer();
+?>
---
+++ wp-content/themes/UD_theme/templates/template_savoir-faire.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Template Name: Savoir faire
+*/
+get_header();
+?>
+
+<p>Page Accueil</p>
+
+<?php
+get_footer();
+?>
