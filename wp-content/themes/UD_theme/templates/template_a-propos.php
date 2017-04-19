2ecaf92933ef598fa16ce28ed17dd33a138e45e9
julien da silva <julien.da-silva@hetic.netgit>
2017-04-19 15:51:56 +0200

    add partenaires

--- wp-content/themes/UD_theme/templates/template_a-propos.php
+++ wp-content/themes/UD_theme/templates/template_a-propos.php
@@ -11,18 +11,25 @@ get_header();
     <span class="text-important">Notre entreprise est à votre écoute</span> afin de discuter de votre projet de travaux. Nos devis sont en fourniture et pose (sauf demande contraire de votre part). Ainsi U&D gére l’intégralité de vos travaux, approvisionne et livre tous les matériels sur le chantier.</p>
 <p class="paragraphe">Here goes the content Here goes the content Here goes the content Here goes the content Here goes the content</p>
 
-<ul class="flex-container">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-lapeyre.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-legrand.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-leroymerlin.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-lapeyre.png" alt="">
-</ul>
-<ul class="flex-container">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-point-p.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-ariston.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-rexel.png" alt="">
-    <img class="flex-item" src="<?php bloginfo('template_url'); ?>/images/partenaires/logo-point-p.png" alt="">
-</ul>
+
+
+
+<section>
+    <ul class="flex-container">
+        <div class="flex-item partenaire-1"></div>
+        <div class="flex-item partenaire-2"></div>
+        <div class="flex-item partenaire-3"></div>
+        <div class="flex-item partenaire-4"></div>
+    </ul>
+    <ul class="flex-container">
+        <div class="flex-item partenaire-5"></div>
+        <div class="flex-item partenaire-6"></div>
+        <div class="flex-item partenaire-7"></div>
+        <div class="flex-item partenaire-8"></div>
+
+    </ul>
+</section>
+
 <?php
 get_footer();
 ?>
