ef813f8fd7540d1a73a9140ce80c0f53842ee314
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-04-19 17:28:23 +0200

    Début inté page 'a-propos'

--- wp-content/themes/UD_theme/templates/template_a-propos.php
+++ wp-content/themes/UD_theme/templates/template_a-propos.php
@@ -4,12 +4,52 @@
 */
 get_header();
 ?>
+<!-- decoration block on the background -->
+<div class="block-deco-grey"></div>
+<!-- Content centered with 60px margin left and right -->
+<div class="centered-block content-a-propos">
+    <div class="project-a-propos">
+        <div class="project-img">
 
-<p class="title">L’entreprise U&G est une équipe d’experts à votre écoute, <span class="text-important">qui vous conseille et vous oriente dans vos choix techniques</span> et répond au mieux à vos besoins</p>
-<p class="paragraphe">Spécialisée dans la rénovation complète ou partielle d’appartement et de maison, notre équipe d’ouvriers professionnels du bâtiment intervient pour tous les travaux nécessaires à l’intérieur de votre habitât.
+        </div>
+        <div class="project-description">
+            <span class="small-title">Confiez-nous votre projet </span>
+            <p>Vous avez un projet de construction, de
+                rénovation ou d’aménagement ?</p>
+            <button>Nous contacter</button>
+        </div>
+    </div>
 
-    <span class="text-important">Notre entreprise est à votre écoute</span> afin de discuter de votre projet de travaux. Nos devis sont en fourniture et pose (sauf demande contraire de votre part). Ainsi U&D gére l’intégralité de vos travaux, approvisionne et livre tous les matériels sur le chantier.</p>
-<p class="paragraphe">Here goes the content Here goes the content Here goes the content Here goes the content Here goes the content</p>
+    <div class="content-text">
+        <div class="title a-propos-title">
+            <p>L’entreprise U&G est une équipe d’experts à votre écoute, <span class="text-important">qui vous conseille et vous oriente dans vos choix techniques</span> et répond au mieux à vos besoins</p>
+        </div>
+        <div class="wrapper-block">
+            <div>
+                <div class="block-p">
+                    <p class="paragraphe">Spécialisée dans la rénovation complète ou partielle d’appartement et de maison, notre équipe d’ouvriers professionnels du bâtiment intervient pour tous les travaux nécessaires à l’intérieur de votre habitât.</p>
+                </div>
+                <div class="block-p">
+                    <p class="paragraphe">afin de discuter de votre projet de travaux. Nos devis sont en fourniture et pose (sauf demande contraire de votre part). Ainsi U&D gére l’intégralité de vos travaux, approvisionne et livre tous les matériels sur le chantier.<span class="text-important">Notre entreprise est à votre écoute</span></p>
+                </div>
+            </div>
+            <div>
+                <div class="block-p">
+                    <p class="paragraphe">En début de chantier vous complétez un document dans lequel vous indiquez vos choix de matériel (carrelages, sanitaires, robinetteries, parquet etc..).</p>
+                </div>
+                <div class="block-p">
+                    <p class="paragraphe">Réaliser des travaux de rénovation dans une maison ou un appartement est une chose sérieuse. Les budgets engagés sont souvent importants et il est indispensable de travailler avec une entreprise de rénovation sérieuse qui présente <span class="text-important">toutes les garanties nécessaires.</span></p>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+
+
+
+</div><!--End centered-block content-a-propos-->
 
 <?php
 get_footer();
