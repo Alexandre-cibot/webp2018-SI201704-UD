b8a881ab43fa604646b11194c40887b7063ce7ed
julien da silva <julien.da-silva@hetic.netgit>
2017-04-19 16:34:38 +0200

    finish partenaire part

--- wp-content/themes/UD_theme/templates/template_a-propos.php
+++ wp-content/themes/UD_theme/templates/template_a-propos.php
@@ -15,19 +15,21 @@ get_header();
 
 
 <section>
-    <ul class="flex-container">
+    <h2 class="title text-center title-partenaire">Ils nous ont fait <span class="text-important">confiance</span></h2>
+    <p class="paragraphe partenaire-paragraphe">Afin d’assurer un travail de qualité, notre société s’est allié des partenaires nationalement connus. Vous bénéficiez ainsi, en plus de la qualité de nos services et de nos prestations, de la qualité des produits de ces grandes marques.</p>
+    <div class="flex-container">
         <div class="flex-item partenaire-1"></div>
         <div class="flex-item partenaire-2"></div>
         <div class="flex-item partenaire-3"></div>
         <div class="flex-item partenaire-4"></div>
-    </ul>
-    <ul class="flex-container">
+    </div>
+    <div class="flex-container">
         <div class="flex-item partenaire-5"></div>
         <div class="flex-item partenaire-6"></div>
         <div class="flex-item partenaire-7"></div>
         <div class="flex-item partenaire-8"></div>
 
-    </ul>
+    </div>
 </section>
 
 <?php
