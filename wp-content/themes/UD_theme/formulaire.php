4e274777a60a92be2d39570583095f1b011a3ab4
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-04-20 16:28:25 +0200

    Ajout du formulaire d'envoie numero de telephone

---
+++ wp-content/themes/UD_theme/formulaire.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Template Name: formulaire
+*/
+
+if (trim($_POST['phone']) === "" OR !is_numeric(trim($_POST['phone']))) {
+    header('location:' . get_site_url());
+}
+
+$phone_number = $_POST['phone'];
+
+// the message
+$msg = "A potential client would like to be call for futher information. \nPhone : " . $phone_number . "\n Send by UDwebsite.com";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("julien@citytaps.org","Recall potential client",$msg);
+
+header('location: ' . get_site_url());
