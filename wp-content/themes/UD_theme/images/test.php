0978188edb90bd4b5e78db61888f8c5f97c25c8e
Alexandre Cibot <alexandre.cibot@gmail.com>
2017-04-20 11:24:33 +0200

    Ajout du contenue dynamique 'page A propos'

---
+++ wp-content/themes/UD_theme/images/test.php
@@ -0,0 +1,1 @@
+<p>JTOTOTOOTO test.php</p>
--- wp-content/themes/UD_theme/templates/template_a-propos.php
+++ wp-content/themes/UD_theme/templates/template_a-propos.php
@@ -21,24 +21,25 @@ get_header();
     </div>
 
     <div class="content-text">
+
         <div class="title a-propos-title">
-            <p>L’entreprise U&G est une équipe d’experts à votre écoute, <span class="text-important">qui vous conseille et vous oriente dans vos choix techniques</span> et répond au mieux à vos besoins</p>
+            <p><?php the_field('titre'); ?></p>
         </div>
         <div class="wrapper-block flex-container">
             <div>
                 <div class="block-p">
-                    <p class="paragraphe">Spécialisée dans la rénovation complète ou partielle d’appartement et de maison, notre équipe d’ouvriers professionnels du bâtiment intervient pour tous les travaux nécessaires à l’intérieur de votre habitât.</p>
+                    <p class="paragraphe"><?php the_field('paragraphe_1');?></p>
                 </div>
                 <div class="block-p">
-                    <p class="paragraphe"><span class="text-important"> Notre entreprise est à votre écoute</span> <br>Afin de discuter de votre projet de travaux. Nos devis sont en fourniture et pose (sauf demande contraire de votre part). Ainsi U&D gére l’intégralité de vos travaux, approvisionne et livre tous les matériels sur le chantier.</p>
+                    <p class="paragraphe"><?php the_field('paragraphe_2');?></p>
                 </div>
             </div>
             <div>
                 <div class="block-p">
-                    <p class="paragraphe">En début de chantier vous complétez un document dans lequel vous indiquez vos choix de matériel (carrelages, sanitaires, robinetteries, parquet etc..).</p>
+                    <p class="paragraphe"><?php the_field('paragraphe_3');?></p>
                 </div>
                 <div class="block-p">
-                    <p class="paragraphe">Réaliser des travaux de rénovation dans une maison ou un appartement est une chose sérieuse. Les budgets engagés sont souvent importants et il est indispensable de travailler avec une entreprise de rénovation sérieuse qui présente <span class="text-important">toutes les garanties nécessaires.</span></p>
+                    <p class="paragraphe"><?php the_field('paragraphe_4');?></p>
                 </div>
             </div>
         </div>
@@ -56,7 +57,7 @@ get_header();
                     <div class="picto"></div>
                     <div class="stat-text-wrapper">
                         <div class="stat-title">Nos réalisations</div>
-                        <div class="stat-numbers">+ de 200 projets</div>
+                        <div class="stat-numbers"><?php the_field('nombre_projets');?></div>
                     </div>
                 </div>
             </div>
@@ -66,14 +67,14 @@ get_header();
                     <div class="picto"></div>
                     <div class="stat-text-wrapper">
                         <div class="stat-title">Une équipe de professionnels</div>
-                        <div class="stat-numbers">6 employés</div>
+                        <div class="stat-numbers"><?php the_field('nombre_employes');?></div>
                     </div>
                 </div>
                 <div class="stat-block">
                     <div class="picto"></div>
                     <div class="stat-text-wrapper">
                         <div class="stat-title">Notre secteur d’intervention</div>
-                        <div class="stat-numbers">5 départements</div>
+                        <div class="stat-numbers"><?php the_field('nombre_departements');?></div>
                     </div>
                 </div>
             </div>
