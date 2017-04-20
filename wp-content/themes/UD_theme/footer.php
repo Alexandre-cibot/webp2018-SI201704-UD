3df5abbfa74ebacfdf3c28297ee9d324ba23d4d0
Enora Gathignol <enora.gathignol@orange.fr>
2017-04-20 09:50:01 +0200

    add footer and mention legale pages

--- wp-content/themes/UD_theme/footer.php
+++ wp-content/themes/UD_theme/footer.php
@@ -1,6 +1,76 @@
 <footer>
-
-</footer>
+   <div class="bandeauReassurance">
+    <div class="blockk">
+     <div class="bandeauReassurance_block">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto1.svg";?>> </div>
+       <div class="sentence">
+         <p>DEVIS DÉTAILLÉ<br><span>Gratuit</span></p>
+        </div>
+     </div>
+     
+     <div class="bandeauReassurance_block">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto2.svg";?>></div>
+       <div class="sentence">
+         <p>DÉLAIS DE TRAVAUX<br><span>Respectés</span></p>
+        </div>
+     </div>
+     
+     <div class="bandeauReassurance_block">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto3.svg";?>></div>
+       <div class="sentence">
+         <p>MATÉRIAUX DE<br><span>Qualité</span></p>
+        </div>
+     </div>
+   </div>
+    <div class="blockk">
+     <div class="bandeauReassurance_block">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto4.svg";?>></div>
+       <div class="sentence">
+         <p>ENSEMBLE DE<br><span>Garanties</span></p>
+        </div>
+     </div>
+     
+     <div class="bandeauReassurance_block">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto5.svg";?>></div>
+       <div class="sentence">
+         <p>TOTALE<br><span>Transparence</span></p>
+        </div>
+     </div>
+     
+     <div class="bandeauReassurance_block last">
+       <div class="picto"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/picto6.svg";?>></div>
+       <div class="sentence">
+         <p>APRÈS<br><span>Suivi régulier</span></p>
+        </div>
+     </div>
+     </div>
+   </div>
+   
+   <div class="blockContact">
+    <div class="blockContact_img"><img src=<?=get_stylesheet_directory_uri() . "/images/footer/line.svg";?>></div>
+     <div class="blockContact_block">
+      <div>
+       <p class="blockContact_block-titre">Contactez-nous</p><br>
+       <p class="blockContact_block-txt">Société U&amp;D<br>
+12 rue des petites écuries<br>
+75010 - PARIS</p><br>
+    <p class="blockContact_block-txt">contact@p&amp;d.com</p><br>
+    <p class="blockContact_block-txt">01.23.48.04.93</p>
+    </div>
+     </div>
+     <div class="blockContact_block">
+      <div>
+       <p class="blockContact_block-titre">Besoin de plus d’informations ?</p><br>
+       <p class="blockContact_block-txt">Envoyez nous un message pour plus d’informations ou pour un devis. Nos devis sont réalisés gratuitement par notre société. Un récapitulatif pièce par pièce et corps d’état par corps d’état vous est proposé en fin de devis.</p>
+       <br>
+     <div class="blockContact_btn"><a href="">Nous contacter</a></div>
+     </div>
+     
+     </div>
+   </div>
+   
+ </footer>
+ 
 
 </body>
 </html>
---
+++ wp-content/themes/UD_theme/templates/template_mentions-legales.php
@@ -0,0 +1,32 @@
+
+<?php get_header(); /* Template Name: Mentions legales */ ?>
+
+
+<div class="ml">
+   <div class="ml_blockBG">
+     <img src="line2.svg">
+   </div>
+   <div class="ml_blockTXT">
+   <h2>Mentions légales</h2>
+   <h3>Coordonnées</h3>
+   <p>La société U&amp;D (ci-après la « Société » ) est une société anonyme à conseil d’administration (SA) au capital de 100.000,00 euros, dont le siège social est , immatriculée sous le numéro 00 000 000 000 000.
+Les sociétés U&amp;D France &amp; SNC GSB LEROY MERLIN sont certifiées pour les activités de mise en vente, vente et de conseil à l’utilisation des produits phytopharmaceutiques, sous les numéros d’agrément suivants.
+La société U&amp;D est l’éditeur du site Internet www.u&amp;d.fr (ci-après le « Site » ) et Monsieur A. en est le directeur de publication en sa qualité de directeur Internet France. 
+Le Site est hébergé par la société IBM SoftLayer / Bluemix, 7 rue Petit 92110 Clichy, France.
+Pour toute question, nous vous remercions de bien vouloir vous adresser au Service Clientèle de U&amp;D :
+U&amp;D - Service Clientèle 
+Téléphone : 00 00 00 00 00
+Email : contact@u&amp;d.fr
+L’accès au Site ainsi que l’utilisation de son contenu s’effectue dans le cadre des mentions d’utilisation décrites ci-après (ci-après les « Mentions légales »). Le fait d’accéder et de naviguer sur le Site constitue de votre part une acceptation sans réserve des précisions suivantes :</p>
+<br><br><br><br>
+   <h3>Droits de propriété intellectuelle</h3>
+   <p>La Société est titulaire du nom de domaine www.u&amp;d.fr . Le Site est une oeuvre de l’esprit protégée par les lois de la propriété intellectuelle. Le Site dans son ensemble et chacun des éléments qui le composent (tels que textes, arborescences, logiciels, animations, photographies, illustrations, schémas, logos) sont la propriété exclusive de la Société qui est seule habilitée à utiliser les droits de propriété intellectuelle y afférents.
+L’usage de tout ou partie du Site, notamment par téléchargement, reproduction, transmission, représentation ou diffusion à d’autres fins que pour votre usage personnel et privé dans un but non commercial est strictement interdit. La violation de ces dispositions vous soumet aux sanctions prévues tant par le Code de la Propriété Intellectuelle et le Code Pénal au titre notamment de la contrefaçon de droit d’auteur et du droit des marques, que par le Code Civil en matière de responsabilité civile.
+Les marques et noms de domaine ainsi que les dessins et modèles qui apparaissent sur le Site sont la propriété exclusive de la Société. La création de liens hypertextes vers le Site ne peut être faite qu’avec l’autorisation écrite et préalable de la Société, laquelle autorisation peut être révoquée à tout moment.
+Tous les sites ayant un lien hypertexte vers le Site ne sont pas sous le contrôle de la Société et celle-ci décline par conséquent toute responsabilité (notamment éditoriale) concernant l’accès et le contenu de ces sites.
+La Société est libre de modifier, à tout moment et sans préavis, le contenu du Site.</p>
+
+</div>
+ </div>
+ 
+<?php get_footer();?>
