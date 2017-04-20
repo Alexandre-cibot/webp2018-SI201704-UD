825c96bb5a72aff3bb9ed28ff0d742940e7bf583
Anthony <lumpastyle@gmail.com>
2017-04-20 13:13:19 +0200

    maj header

--- wp-content/themes/UD_theme/header.php
+++ wp-content/themes/UD_theme/header.php
@@ -6,12 +6,34 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=<?= get_stylesheet_directory_uri() . "/css/style.css"; ?> >
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css">
-    <script defer src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js"></script>
-    <script defer src=<?php echo get_stylesheet_directory_uri() . "/script/script.js";?>></script>
     <title>Document</title>
 </head>
 <body>
+<div class="menu">
+    <img src=<?= get_stylesheet_directory_uri() . "/images/picto-cross.svg"?> alt="close">
+    <nav>
+        <ul>
+            <a href=""><li class="active">accueil</li></a>
+            <a href=""><li>savoir-faire</li></a>
+            <a href=""><li>à propos</li></a>
+            <a href=""><li>contact</li></a>
+        </ul>
+    </nav>
+</div>
+
+<header>
+    <img src=<?= get_stylesheet_directory_uri() . "/images/picto-burger.svg" ?> alt="burger">
+    <img src=<?= get_stylesheet_directory_uri() . "/images/logo_black.svg" ?> alt="logo U&D black">
+    <img src=<?= get_stylesheet_directory_uri() . "/images/logo_white.svg" ?> alt="logo U&D white">
+    <nav>
+        <ul>
+            <a href=""><li class="bt-devis"><img src=<?= get_stylesheet_directory_uri() . "/images/picto-devis.svg" ?> alt=""> Devis</li></a>
+            <a href=""><li class="bt-devis mobile"><img src=<?= get_stylesheet_directory_uri() . "/images/picto-devis.svg" ?> alt=""></li></a>
+            <a href=""><li>à propos</li></a>
+            <a href=""><li>savoir-faire</li></a>
+            <a href=""><li class="active">accueil</li></a>
+        </ul>
+    </nav>
+</header>
+
+<div class="clear"></div>
---
+++ wp-content/themes/UD_theme/script/script.js
@@ -0,0 +1,9 @@
+$('img[alt="burger"]').on("click", function(){
+    $('.menu').toggleClass('active');
+});
+
+$('img[alt="close"]').on("click", function(){
+    $('.menu').toggleClass('active');
+});
+
+alert("c'est la polisse");
